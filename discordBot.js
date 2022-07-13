78c071023cbd6fa0cc4924ab2396165d7c69c2f4
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-07-13 18:56:48 +0700

    Initial build...

--- discordBot.js
+++ discordBot.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js'); // Discord in general
-// const fetch = require('node-fetch'); // HTTP request
+const fetch = require('node-fetch'); // HTTP request
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-// API_URL = ''; // URL
+API_URL = 'https://api-inference.huggingface.co/models/YumeAyasaki/DialoGPT-small-yumebot'; // URL
 
 // For ready
 client.on('ready', () => {
@@ -11,7 +11,7 @@ client.on('ready', () => {
 })
 
 // Message
-client.on('message', async message => { // Request html => API => async
+client.on('messageCreate', async message => { // Request html => API => async
     if (message.author.bot) { // Ignore bot
         return;
     };
@@ -19,26 +19,25 @@ client.on('message', async message => { // Request html => API => async
     // Form to send API
     const payload = { // payload
         inputs: {
-            text: message.context
+            text: message.content
         }
     };
-    // const headers = { // header
-    //     'Authorization': 'Bearer ' + process.env.HUGGINGFACE_TOKEN
-    // };
+    const headers = { // header
+        'Authorization': 'Bearer ' + process.env.HUGGINGFACE_TOKEN
+    };
 
     // message.channel.messageCreate();
     await message.channel.sendTyping();
 
-    // const response = await fetch(API_URL, {method: 'post', body: JSON.stringify(payload), headers: headers});
-    // const data = await response.json();
-    // let botResponse = '';
-    // if (data.hasOwnProperty('generated_text')) {
-    //     botResponse = data.generated_text;
-    // } else if (data.hasOwnProperty('error')) { // error condition
-    //     botResponse = data.error;
-    // }
-    botResponse = "I catch that...";
-    message.reply(botResponse);
+    const response = await fetch(API_URL, {method: 'post', body: JSON.stringify(payload), headers: headers});
+    const data = await response.json();
+    let botResponse = '';
+    if (data.hasOwnProperty('generated_text')) {
+        botResponse = data.generated_text;
+    } else if (data.hasOwnProperty('error')) { // error condition
+        botResponse = data.error;
+    }
+    message.channel.send(botResponse);
 })
 
 client.login(process.env.DISCORD_TOKEN);
